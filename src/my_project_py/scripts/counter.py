1fd7502d9cf0fe6c8b1483f2903ae35daf1c7650
jefferson-norberto2 <jmn@cin.ufpe.br>
2023-02-01 09:47:38 -0300

    service python

--- src/my_project_py/scripts/counter.py
+++ src/my_project_py/scripts/counter.py
@@ -2,15 +2,18 @@
 
 import rospy
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty, EmptyResponse
 
 class Counter:
     def __init__(self):
         self.count = 0
         self.pub_interval = 3
+
         # nome do tópico, tipo de mensagen e a função callback e pode definir o tamanho do buffer
         self.num_sub = rospy.Subscriber("/number", Float64, self.number_callback, queue_size=10)
         self.count_pub = rospy.Publisher("/current_count", Float64, queue_size=10)
         self.timer_pub = rospy.Timer(rospy.Duration(self.pub_interval), self.timer_callback)
+        self.reset_srv = rospy.Service("reset_counter", Empty, self.reset_srv_callback)
         rospy.loginfo("Counter inicialized")
     
     def number_callback(self, msg):
@@ -21,6 +24,11 @@ class Counter:
         msg = Float64()
         msg.data = self.count
         self.count_pub.publish(msg)
+    
+    def reset_srv_callback(self, request):
+        self.count = 0
+        rospy.loginfo("Reseted count")
+        return EmptyResponse()
 
 if __name__ == '__main__':
     try:
