607a3211fe93debb7bbeffc342ab3afa4f80753c
jefferson-norberto2 <jmn@cin.ufpe.br>
2023-02-01 09:54:44 -0300

    client service python

--- src/my_project_py/scripts/number_publisher.py
+++ src/my_project_py/scripts/number_publisher.py
@@ -2,15 +2,21 @@
 
 import rospy
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 
 class NumberPublisher:
 
     def __init__(self):
         self.number = 3.1
         self.publisher_interval = 1.5
+        self.reset_interval = 7
 
         self.number_publisher = rospy.Publisher("/number", Float64, queue_size=10)
+        
         self.timer_pub = rospy.Timer(rospy.Duration(self.publisher_interval), self.timer_callback)
+        self.timer_reset = rospy.Timer(rospy.Duration(self.reset_interval), self.reset_callback)
+        
+        self.client_reset = rospy.ServiceProxy("reset_counter", Empty, )
 
         rospy.loginfo("Inicialized")
 
@@ -19,6 +25,10 @@ class NumberPublisher:
         msg.data = self.number
         self.number_publisher.publish(msg)
         rospy.loginfo("Published value: " + str(msg.data))
+    
+    def reset_callback(self, event):
+        rospy.loginfo("Solicitação de reset da contagem")
+        self.client_reset()
         
         
 
