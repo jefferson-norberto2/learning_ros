7ea7370ad9de5bdacf26b59e3f4c8164a82802d1
jefferson-norberto2 <jmn@cin.ufpe.br>
2023-02-13 14:50:10 -0300

    custom msg py

--- src/my_project_py/scripts/counter.py
+++ src/my_project_py/scripts/counter.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Float64
 from std_srvs.srv import Empty, EmptyResponse
+from my_project_msgs.msg import CounterHistory
 
 class Counter:
     def __init__(self):
@@ -22,15 +23,21 @@ class Counter:
             if rospy.has_param("initial_count"):
                 rospy.delete_param("initial_count")
              
+        self.last_count = self.count
+        self.cycles = 0
 
         # nome do tópico, tipo de mensagen e a função callback e pode definir o tamanho do buffer
         self.num_sub = rospy.Subscriber("/number", Float64, self.number_callback, queue_size=10)
         self.count_pub = rospy.Publisher("/current_count", Float64, queue_size=10)
         self.timer_pub = rospy.Timer(rospy.Duration(self.pub_interval), self.timer_callback)
         self.reset_srv = rospy.Service("reset_counter", Empty, self.reset_srv_callback)
+        self.history_pub = rospy.Publisher("/history_count", CounterHistory, queue_size=10)
+        
         rospy.loginfo("Counter inicialized")
     
     def number_callback(self, msg):
+        self.cycles += 1
+        self.last_count = self.count
         self.count = self.count + msg.data
         rospy.loginfo("The current counter: " + str(self.count))
     
@@ -38,6 +45,12 @@ class Counter:
         msg = Float64()
         msg.data = self.count
         self.count_pub.publish(msg)
+
+        history_msg = CounterHistory()
+        history_msg.current_value = self.count
+        history_msg.last_value = self.last_count
+        history_msg.cycles = self.cycles
+        self.history_pub.publish(history_msg)
     
     def reset_srv_callback(self, request):
         self.count = 0
